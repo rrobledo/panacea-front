38ed1a22cfa89114a306b3c2a69f30f7c280fcfd
rrobledo <raul.osvaldo.robledo@gmail.com>
2024-07-31 10:03:55 -0300

    Adding new DataGrid Componente

---
+++ src/components/DataGrid.tsx
@@ -0,0 +1,79 @@
+import { Link } from "react-router-dom";
+import { Table, Button, Space, Modal, Spin } from "antd";
+import { useEffect, useRef, useState } from "react";
+import { ColDef, ColGroupDef } from "ag-grid-community";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-quartz.css";
+
+export type useRunOnceProps = {
+  fn: () => any;
+  sessionKey?: string;
+};
+
+const useRunOnce: React.FC<useRunOnceProps> = ({ fn, sessionKey }) => {
+  const triggered = useRef<boolean>(false);
+
+  useEffect(() => {
+    const hasBeenTriggered = sessionKey
+      ? sessionStorage.getItem(sessionKey)
+      : triggered.current;
+
+    if (!hasBeenTriggered) {
+      fn();
+      triggered.current = true;
+
+      if (sessionKey) {
+        sessionStorage.setItem(sessionKey, "true");
+      }
+    }
+  }, [fn, sessionKey]);
+
+  return null;
+};
+
+function DataGrid(props: any) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [rowData, setRowData] = useState([]);
+  const columns = props.columns != undefined ? props.columns : "id";
+  const rowIdName = props.rowIdName != undefined ? props.rowIdName : "id";
+  const resourceParent =
+    props.resourceParent != undefined ? `${props.resourceParent}/` : "";
+
+  const [colDefs, setColDefs] = useState<ColGroupDef[]>(columns);
+  const defaultColDef: ColDef = { width: 70 };
+
+  const getList = () => {
+    props.ds.getList(`${resourceParent}${props.resource}`).then((res: any) => {
+      setRowData(res.data);
+      setIsLoading(false);
+    });
+  };
+
+  useEffect(() => {
+    getList();
+  }, []);
+
+  useRunOnce({
+    fn: () => {},
+  });
+
+  return (
+    <div>
+      <Spin tip="Loading" size="large" spinning={isLoading}>
+        <div
+          className={"ag-theme-quartz"}
+          style={{ width: "100%", height: 700 }}
+        >
+          <AgGridReact
+            rowData={rowData}
+            columnDefs={colDefs}
+            defaultColDef={defaultColDef}
+          />
+        </div>
+      </Spin>
+    </div>
+  );
+}
+
+export default DataGrid;
--- src/reportes/costos/planning2024.tsx
+++ src/reportes/costos/planning2024.tsx
@@ -1,8 +1,143 @@
 import { Table } from "antd";
 import DataList from "../../components/DataList";
+import DataGrid from "../../components/DataGrid";
 
 function Planning2024(props: any) {
   const columns = [
+    {
+      headerName: "",
+      children: [
+        {
+          field: "producto",
+          width: 200,
+          headerName: "Producto",
+          pinned: "left",
+        },
+      ],
+    },
+    {
+      headerName: "Enero",
+      children: [
+        {
+          headerName: "Plan",
+          field: "enero_plan",
+        },
+        {
+          headerName: "Real",
+          field: "enero_real",
+        },
+      ],
+    },
+    {
+      headerName: "Febero",
+      children: [
+        {
+          headerName: "Plan",
+          field: "febrero",
+        },
+        {
+          headerName: "Real",
+          field: "febrero_real",
+        },
+      ],
+    },
+    {
+      headerName: "Marzo",
+      children: [
+        {
+          headerName: "Plan",
+          field: "marzo",
+        },
+        {
+          headerName: "Real",
+          field: "marzo_real",
+        },
+      ],
+    },
+    {
+      headerName: "Abril",
+      children: [
+        {
+          headerName: "Plan",
+          field: "abril",
+        },
+        {
+          headerName: "Real",
+          field: "abril_real",
+        },
+      ],
+    },
+    {
+      headerName: "Mayo",
+      children: [
+        {
+          headerName: "Plan",
+          field: "mayo",
+        },
+        {
+          headerName: "Corr",
+          field: "mayo_corregido",
+        },
+        {
+          headerName: "Real",
+          field: "mayo_real",
+        },
+      ],
+    },
+    {
+      headerName: "Junio",
+      children: [
+        {
+          headerName: "Plan",
+          field: "junio",
+        },
+        {
+          headerName: "Corr",
+          field: "junio_corregido",
+        },
+        {
+          headerName: "Real",
+          field: "junio_real",
+        },
+      ],
+    },
+    {
+      headerName: "Julio",
+      children: [
+        {
+          headerName: "Plan",
+          field: "julio",
+        },
+        {
+          headerName: "Corr",
+          field: "julio_corregido",
+        },
+        {
+          headerName: "Real",
+          field: "julio_real",
+        },
+      ],
+    },
+    {
+      headerName: "Agosto",
+      children: [
+        {
+          headerName: "Plan",
+          field: "agosto",
+        },
+        {
+          headerName: "Corr.",
+          field: "agosto_corregido",
+        },
+        {
+          headerName: "Real",
+          field: "agosto_real",
+        },
+      ],
+    },
+  ];
+
+  const columns1 = [
     {
       title: "Producto",
       dataIndex: "producto",
@@ -105,42 +240,16 @@ function Planning2024(props: any) {
       dataIndex: "agosto_corregido",
       key: "agosto_corregido",
     },
-    // {
-    //   title: "Septiembre",
-    //   dataIndex: "septiembre",
-    //   key: "septiembre",
-    // },
-    // {
-    //   title: "Octubre",
-    //   dataIndex: "octubre",
-    //   key: "octubre",
-    // },
-    // {
-    //   title: "Noviembre",
-    //   dataIndex: "noviembre",
-    //   key: "noviembre",
-    // },
-    // {
-    //   title: "Diciembre",
-    //   dataIndex: "diciembre",
-    //   key: "diciembre",
-    // },
   ];
 
   return (
     <>
       <b>PLANNING 2024</b>
-      <DataList
+      <DataGrid
         ds={props.ds}
         resource={props.resource}
         columns={columns}
-        addAction={false}
-        delAction={false}
-        editAction={false}
-        viewAction={false}
-        rowIdName="ref_id"
-      />
-      ;
+      ></DataGrid>
     </>
   );
 }
